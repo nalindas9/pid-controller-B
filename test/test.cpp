f512930b4cc2f87cb0767e4bcbd473810d56ddda
vishnuu95 <vishnuu@terpmail.umd.edu>
2020-09-26 18:27:48 -0400

    added Proportional cpp tests

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,24 @@
 #include <gtest/gtest.h>
+#include <PIDController.h>
+#include <main.cpp>
+#include <limits>
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+TEST(checkCalcVelocity, proportionalCheckTest){
+  double kp = 2.5; 
+  double ki = 0.00; 
+  double kd = 0; 
+  double dt = 0.1; 
+  pidController pidCon = pidController(kp, ki, kd, dt);
+  auto setPoint = 5;
+  auto feedback = 5;
+  ASSERT_DOUBLE_EQ(0.0, pidCon.calculateVelocity(setPoint, feedback));
+  setPoint = 5;
+  feedback = 0;
+  ASSERT_DOUBLE_EQ(12.5, pidCon.calculateVelocity(setPoint, feedback));
+  setPoint = 0;
+  feedback = 5;
+  ASSERT_DOUBLE_EQ(-12.5, pidCon.calculateVelocity(setPoint, feedback));
+  setPoint = numeric_limits<double>::max();
+  feedback = numeric_limits<double>::max();
+  ASSERT_DOUBLE_EQ(0.0, pidCon.calculateVelocity(setPoint, feedback));
 }
