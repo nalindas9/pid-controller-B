3e6b62046854257b15926f70f899a484cd613bdb
Karan Sutradhar <60763520+Karansutradhar@users.noreply.github.com>
2020-09-27 17:57:30 -0400

    Updated with comments

--- app/PIDController.cpp
+++ app/PIDController.cpp
@@ -24,13 +24,19 @@
 #include "PIDController.h"
 
 /**
- * @brief parameterized constructor to initialize the private members
- * @param textInput is the user input text
- * @param stringToSearch is the string to search in the given text
- * @returns location of string in the text in size_t
+ * @brief default constructor of the class pidController
  */
 
 pidController::pidController(){}
+
+/**
+ * @brief parameterized constructorof class pidController to initialize the private members
+ * @param kpValue variable for initializing the member kp
+ * @param kiValue variable for initializing the member ki
+ * @param kdValue variable for initializing the member kd
+ * @param dtValue variable for initializing the member dt
+ * @return none
+ */ 
 
 pidController::pidController(double kpValue, double kiValue, double kdValue, double dtValue) {
     kp = kpValue;
@@ -39,27 +45,98 @@ pidController::pidController(double kpValue, double kiValue, double kdValue, dou
     dt = dtValue;
 }
 
+/**
+ * @brief default destructor which destroys the object of the class pidController
+ */
+
 pidController::~pidController(){}
+
+/**
+ * @brief it is a setter method to set the Kp variable to a new value
+ * @param double k
+ * @return none
+ */
 
 void pidController::setKpGain(double k){}
 
+/**
+ * @brief it is a setter method to set the Ki variable to a new value
+ * @param double k
+ * @return none
+ */
+
 void pidController::setKiGain(double k){}
+
+/**
+ * @brief it is a setter method to set the Kd variable to a new value
+ * @param double k
+ * @return none
+ */
 
 void pidController::setKdGain(double k){}
 
+/**
+ * @brief it is a setter method to set the Dt variable to a new value
+ * @param double dt
+ * @return none
+ */
+
 void pidController::setDtVal(double dt){}
+
+/**
+ * @brief it is a getter method to get the Dt member variable
+ * @param none
+ * @return 0.0
+ */
 
 double pidController::getDtVal(){return 0.0;}
 
+/**
+ * @brief it is a getter method to get the Kp member variable
+ * @param none
+ * @return 0.0
+ */
+
 double pidController::getKpGain(){return 0.0;}
+
+/**
+ * @brief it is a getter method to get the Ki member variable
+ * @param none
+ * @return 0.0
+ */
 
 double pidController::getKiGain(){return 0.0;}
 
+/**
+ * @brief it is a getter method to set the Kd member variable
+ * @param none
+ * @return 0.0
+ */
+
 double pidController::getKdGain(){return 0.0;}
+
+/**
+ * @brief it is a getter method to get the IntergralError variable
+ * @param none
+ * @return 0.0
+ */
 
 double pidController::getIntegralError(){return 0.0;}
 
+/**
+ * @brief it is a reset method to reset the Integral Errror
+ * @param none
+ * @return none
+ */
+
 void pidController::resetIntegralError(){}
+
+/**
+ * @brief it is a method to compute the output velocity
+ * @param double requiredVelocity
+ * @param double actualVelocity
+ * @return 0.0
+ */
 
 double pidController::calculateVelocity(double requiredVelocity, double actualVelocity) {
     return 0.0;
