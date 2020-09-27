432b45d24e7a682b9788a3d6a29260f80ba19dc7
Karan Sutradhar <60763520+Karansutradhar@users.noreply.github.com>
2020-09-27 18:18:17 -0400

    Updating the comments for PIDController.h

--- include/PIDController.h
+++ include/PIDController.h
@@ -1,8 +1,9 @@
-/** @file PIDController.h
-* @author
+/**
+* @file main.cpp
+* @authors
 *
 * Karan Sutradhar (117037272)
-* Vishnuu Appaya Dhanabalan
+* Vishnuu Appaya Dhanabalan (116873314)
 *
 * @version 1.0
 *
@@ -25,6 +26,10 @@
 #include <iostream>
 using namespace std;
 
+/**
+ * @brief it is the declaration of pidController Class
+ */
+
 class pidController
 {
     private:
@@ -35,18 +40,116 @@ class pidController
         double intgrError; // integral of all error over time.  
 
     public:
-        pidController();  // Empty Constructor  
+    
+/**
+ * @brief default constructor of the class pidController
+ */
+    
+        pidController();  // Empty Constructor
+
+/**
+ * @brief parameterized constructorof class pidController to initialize the private members
+ * @param kpValue variable for initializing the member kp
+ * @param kiValue variable for initializing the member ki
+ * @param kdValue variable for initializing the member kd
+ * @param dtValue variable for initializing the member dt
+ * @return none
+ */ 
+    
         pidController(double kpValue, double kiValue, double kdValue, double dtValue); // Value Constructor
+    
+ /**
+ * @brief default destructor which destroys the object of the class pidController
+ */
+    
         ~pidController(); // Destructor
+ 
+ /**
+ * @brief it is a setter method to set the Kp variable to a new value
+ * @param double k
+ * @return none
+ */   
         void setKpGain(double k); // Update Kp Gain on the fly
+  
+ /**
+ * @brief it is a setter method to set the Ki variable to a new value
+ * @param double k
+ * @return none
+ */  
+    
         void setKiGain(double k); // Update Ki Gain on the fly
+    
+ /**
+ * @brief it is a setter method to set the Kd variable to a new value
+ * @param double k
+ * @return none
+ */  
+    
         void setKdGain(double k); // Update Kd Gain on the fly
+
+ /**
+ * @brief it is a setter method to set the Dt variable to a new value
+ * @param double dt
+ * @return none
+ */    
+    
         void setDtVal(double newDt); // Update Dt Val on the fly
+  
+/**
+ * @brief it is a getter method to get the Kp member variable
+ * @param none
+ * @return 0.0
+ */
+    
         double getKpGain(); // Get Kp Gain
+
+ /**
+ * @brief it is a getter method to get the Ki member variable
+ * @param none
+ * @return 0.0
+ */    
+    
         double getKiGain(); // Get Ki Gain
+
+ /**
+ * @brief it is a getter method to set the Kd member variable
+ * @param none
+ * @return 0.0
+ */    
+    
         double getKdGain(); // Get Kd Gain
+  
+  /**
+ * @brief it is a getter method to get the IntergralError variable
+ * @param none
+ * @return 0.0
+ */  
+    
         double getDtVal(); // Get Dt Val
+    
+/**
+ * @brief it is a getter method to get the IntergralError variable
+ * @param none
+ * @return 0.0
+ */
+    
         double getIntegralError(); // Get Integral Error value being tracked. 
+ 
+/**
+ * @brief it is a method to compute the output velocity
+ * @param double requiredVelocity
+ * @param double actualVelocity
+ * @return 0.0
+ */ 
+    
         double calculateVelocity(double requiredVelocity, double actualVelocity); // Compute the PID Output
+    
+/**
+ * @brief it is a reset method to reset the Integral Errror
+ * @param none
+ * @return none
+ */    
         void resetIntegralError(); // Reset the Intergrator error
+    
+    
 };
